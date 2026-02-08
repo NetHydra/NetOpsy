4a40c575729130273686a2295a3cb4a3b4d38671
me-joe <ctf.beginer.joe@gmail.com>
2026-02-08 12:15:00 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -73,7 +73,26 @@ else
 	install_dep
 fi
 
+if [[ ! -e ${BUILD_DIR}/etc/os-release ]]; then
+	echo "Hallo world"
+else
+	read -p "Do you want to clean your current build? (y/n)" RESP
+	if [ "$RESP" = "y" ]; then
+		echo "Rebuilding NetOpsy"
+		if mountpoint -q ${BUILD_DIR}/dev; then
+			umount ${BUILD_DIR}/dev
+			umount ${BUILD_DIR}/dev/pts
+			umount ${BUILD_DIR}/proc
+			umount ${BUILD_DIR}/sys
+			umount ${BUILD_DIR}/run
+		fi
+	else
+		echo "Continue current build"
+		# we will assume the partition has been mounted
+		. $READ_PATH/setup-base-fs.sh
+		. $READ_PATH/helper/netopsy-build-system.sh
+	fi
+fi
+
 mkdir -p ${BUILD_DIR}
 debootstrap --foreign --components main,contrib,non-free,non-free-firmware --arch ${ARCH} $DEBIAN_BRANCH ${BUILD_DIR} $DEBIAN_MIRROR
-. $READ_PATH/setup-base-fs.sh
-. $READ_PATH/helper/netopsy-build-system.sh
